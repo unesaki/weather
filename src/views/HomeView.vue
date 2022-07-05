52b18c0312d3abe21fc4f7b549ff72f80b3a8476
unesaki <unesaki1610@gmail.com>
2022-07-05 13:17:00 +0900

    first-commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home flex">
+    <Card v-for="(data, index) in lists" :key="index" :city="data.city" /> 
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Card from "../components/TheCard";
+
+export default {
+  data() {
+    return {
+      lists: [
+        { city: "sapporo" },
+        { city: "tokyo" },
+        { city: "nagoya" },
+        { city: "osaka" },
+        { city: "fukuoka" },
+        { city: "naha" },
+      ]
+    };
+  },
+  components: {
+    Card
+  }
+};
+</script>
+
+<style scoped>
+body {
+  background: #eee;
+}
+.flex {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+</style>
